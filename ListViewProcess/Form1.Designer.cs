75ca04930294a406ad5033a893fc80cad4ddedbb
Anuarbek <anuarbekzhamanbaev@gmail.com>
2019-08-26 14:09:52 +0600

    Добавьте файлы проекта.

---
+++ ListViewProcess/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ListViewProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup5 = new System.Windows.Forms.ListViewGroup("Name", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup6 = new System.Windows.Forms.ListViewGroup("ID", System.Windows.Forms.HorizontalAlignment.Left);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.IDColumn1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.IDColumn1,
+            this.NameColumn,
+            this.columnHeader1,
+            this.columnHeader2});
+            listViewGroup5.Header = "Name";
+            listViewGroup5.Name = "Name";
+            listViewGroup6.Header = "ID";
+            listViewGroup6.Name = "ID";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup5,
+            listViewGroup6});
+            this.listView1.Location = new System.Drawing.Point(90, 36);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(595, 288);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // IDColumn1
+            // 
+            this.IDColumn1.Text = "ID";
+            this.IDColumn1.Width = 125;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.Text = "Name";
+            this.NameColumn.Width = 83;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ThreadCount";
+            this.columnHeader1.Width = 121;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "HandleCount";
+            this.columnHeader2.Width = 129;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save to File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader IDColumn1;
+        private System.Windows.Forms.ColumnHeader NameColumn;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ListViewProcess/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListViewProcess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] processList = Process.GetProcesses();
+            listView1.Items.Clear();
+            foreach (var process in processList)
+            {
+                var item = new string[] { process.Id.ToString(), process.ProcessName, process.Threads.Count.ToString(), process.HandleCount.ToString() };
+                ListViewItem row = new ListViewItem(item);             
+                row.Tag = process;
+                listView1.Items.Add(row);               
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.ShowDialog();
+
+                string fister = save.FileName;
+
+                StreamWriter sw = new StreamWriter(fister);
+
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    for(int i=0; i < item.SubItems.Count; i++)
+                    {
+                        sw.Write(item.SubItems[i].Text);
+                        if (i != item.SubItems.Count - 1)
+                        {
+                            sw.Write(",");
+                        }
+                                                   
+                    }
+                    sw.WriteLine();
+                }
+                sw.Close();
+                MessageBox.Show("File save to: " + fister);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+
+             
+        }
+    }
+}
